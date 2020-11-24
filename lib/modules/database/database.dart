2d8a8c626421ca7999c6e934ab4db4a004600f19
abduallah <abdullah.mansour97@gmail.com>
2020-11-24 19:40:23 +0200

    database v2

--- lib/modules/database/database.dart
+++ lib/modules/database/database.dart
@@ -1,7 +1,10 @@
+
 import 'package:calculator/shared/components/components.dart';
+import 'package:conditional_builder/conditional_builder.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:sqflite/sqlite_api.dart';
 
@@ -13,25 +16,46 @@ class WaitDatabase extends StatefulWidget {
 class _WaitDatabaseState extends State<WaitDatabase> {
   var nameController = TextEditingController();
   var numberController = TextEditingController();
+  var database;
+  List<Map> list;
 
   @override
   void initState() {
     super.initState();
 
-    createDatabase();
+    createDatabase().then((value)
+    {
+      if(value != null)
+        database = value;
+
+      getUsers(
+        database: value,
+      ).then((value)
+      {
+        this.list = value;
+        setState(() {
+
+        });
+      });
+    });
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context)
+  {
     return Scaffold(
       appBar: AppBar(
         title: Text('Wait List'),
       ),
-      body: ListView.separated(
-        physics: BouncingScrollPhysics(),
-        itemBuilder: (context, index) => buildItem(),
-        separatorBuilder: (context, index) => buildSeparator(),
-        itemCount: 15,
+      body: ConditionalBuilder(
+        condition: this.list != null,
+        builder: (context) => ListView.separated(
+          physics: BouncingScrollPhysics(),
+          itemBuilder: (context, index) => buildItem(this.list[index]),
+          separatorBuilder: (context, index) => buildSeparator(),
+          itemCount: this.list.length,
+        ),
+        fallback: (context) => Center(child: CircularProgressIndicator()),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -58,7 +82,26 @@ class _WaitDatabaseState extends State<WaitDatabase> {
                   ),
                   defaultButton(
                     text: 'Add',
-                    function: () {},
+                    function: ()
+                    {
+                      String name = nameController.text;
+                      String number = numberController.text;
+
+                      if(name.isEmpty || number.isEmpty)
+                      {
+                        Fluttertoast.showToast(msg: 'please enter a valid data');
+                        return;
+                      }
+
+                      insertUser(
+                        database: this.database,
+                        name: name,
+                        number: number,
+                      ).then((value)
+                      {
+                        Navigator.pop(context);
+                      });
+                    },
                   ),
                   SizedBox(
                     height: 10.0,
@@ -81,14 +124,14 @@ class _WaitDatabaseState extends State<WaitDatabase> {
     );
   }
 
-  Widget buildItem() => Padding(
+  Widget buildItem(Map item) => Padding(
         padding: EdgeInsets.all(15.0),
         child: Row(
           children: [
             CircleAvatar(
               radius: 30.0,
               child: Text(
-                '4',
+                '${item['number']}',
                 style: TextStyle(
                   fontWeight: FontWeight.w700,
                   fontSize: 25.0,
@@ -99,7 +142,7 @@ class _WaitDatabaseState extends State<WaitDatabase> {
               width: 20.0,
             ),
             Text(
-              'Ahmed Ali',
+              '${item['name']}',
               style: TextStyle(
                 fontWeight: FontWeight.w700,
                 fontSize: 25.0,
@@ -127,5 +170,21 @@ class _WaitDatabaseState extends State<WaitDatabase> {
         print('database opened');
       },
     );
+  }
+
+  Future<void> insertUser({Database database, String name, String number}) async
+  {
+    await database.transaction((txn) async
+    {
+      await txn.rawInsert('INSERT INTO waiting(name, number) VALUES("$name", $number)').then((value)
+      {
+        print('$value');
+      });
+    });
+  }
+
+  Future<List<Map>> getUsers({Database database}) async
+  {
+    return await database.rawQuery('SELECT * FROM waiting');
   }
 }
